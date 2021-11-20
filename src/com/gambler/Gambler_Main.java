1e30d432ad1af3a045fc1e77c53b6d68282b1e4c
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-21 01:56:03 +0530

    UC7: Continue playing if won added

---
+++ src/com/gambler/Gambler_Main.java
@@ -0,0 +1,18 @@
+package com.gambler;
+
+
+public class Gambler_Main {
+
+public static void main(String[] args) {
+
+
+	Gamler game = new Gamler();
+	int dailyStake = game.dailyStake;
+	System.out.println("Welcome to Gambling game\n\nYou are starting a game with Account balance = $"+ dailyStake);
+	System.out.println("\nFor Month \n\n");
+	
+	game.decidePlaying();
+	
+	}
+
+}
